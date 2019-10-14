c745a129a34b342f880bbb8a45297d507a888cad
崮生 <admin@shenzilong.cn>
2019-10-14 18:38:11 +0800

    要注意，在严格模式下还有一些问题

--- adap/cb-promise.ts
+++ adap/cb-promise.ts
@@ -1,22 +1,34 @@
 import { Parameters } from "../ts-type/type-util"
 
+type b<T> = T extends undefined ? {
+    success: (res: any) => any,
+} : T
+
 type un_success_par<T extends {
     (options: {
         success: (res: any) => any,
         [name: string]: any
     }): any
-}> = Parameters<Parameters<T>[0]['success']>[0];
+}> = Parameters<b<Parameters<T>[0]>['success']>[0];
 
-/** 将选项参数回调风格的函数转为promise */
+
+function assect<T>(par: T | undefined): T {
+    return par!
+}
+
+
+// type BB<T extends infer R> = R
+
+/** 将选项参数回调风格的函数转为promise ,存在一些 问题，当开启全严格模式时，返回的值是未知的 */
 export function adap_promise<T extends {
     (options: {
         /** success类型是必须的，靠这个来获取返回值 */
         success: (res: any) => any,
     }
-    /** 获取函数本就该有的约束，不然没有这句话一些必须的参数在adap_promise的par中就不是必须的，这样他们的类型就对不上 */
-    & Parameters<T>[0]): any
-}>
-    (fn: T, par: Parameters<T>[0]): Promise<un_success_par<T>> {
+        /** 获取函数本就该有的约束，不然没有这句话一些必须的参数在adap_promise的par中就不是必须的，这样他们的类型就对不上 */
+        & Parameters<T>[0]): any
+}>                                  //un_success_par<T>
+    (fn: T, par: Parameters<T>[0]):Promise<un_success_par<T>> {
     return new Promise((resolve, reject) => {
         fn({
             ...par,
@@ -29,3 +41,19 @@ export function adap_promise<T extends {
         })
     })
 }
+
+interface fun2 {
+    ff<T>(par: T | undefined): T
+}
+
+adap_promise(wx.getSetting, {
+    success(a) {
+
+    }
+}).then(_r => {
+
+})
+type a = undefined | {}
+
+type c = b<a>
+
