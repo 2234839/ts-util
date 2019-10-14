6d3859d8b7522711e04802dccca7123ed263fcf9
崮生 <admin@shenzilong.cn>
2019-10-14 13:54:42 +0800

    修改为正确的错误回调

--- adap/cb-promise.ts
+++ adap/cb-promise.ts
@@ -1,19 +1,29 @@
 import { Parameters } from "../ts-type/type-util"
 
+type un_success_par<T extends {
+    (options: {
+        success: (res: any) => any,
+        [name: string]: any
+    }): any
+}> = Parameters<Parameters<T>[0]['success']>[0];
+
 /** 将选项参数回调风格的函数转为promise */
 export function adap_promise<T extends {
     (options: {
-        success: (res: any) => any
-    }): any
+        /** success类型是必须的，靠这个来获取返回值 */
+        success: (res: any) => any,
+    }
+    /** 获取函数本就该有的约束，不然没有这句话一些必须的参数在adap_promise的par中就不是必须的，这样他们的类型就对不上 */
+    & Parameters<T>[0]): any
 }>
-    (fn: T, par: Parameters<T>[0]): Promise<Parameters<Parameters<T>[0]['success']>[0]> {
+    (fn: T, par: Parameters<T>[0]): Promise<un_success_par<T>> {
     return new Promise((resolve, reject) => {
         fn({
             ...par,
             success(res: any) {
                 resolve(res)
             },
-            final(err: any) {
+            fail(err: any) {
                 reject(err)
             }
         })
