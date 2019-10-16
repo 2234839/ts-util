b82f8ec5df2c035001274dafb0229a75ec3a6738
崮生 <admin@shenzilong.cn>
2019-10-16 15:43:13 +0800

    新增一个array类型

--- adap/cb-promise.ts
+++ adap/cb-promise.ts
@@ -16,8 +16,13 @@ function assect<T>(par: T | undefined): T {
     return par!
 }
 
+/** 从联合类型中去剔除某些类型 */
 
-// type BB<T extends infer R> = R
+/** 剔除undefined */
+type wipe_off_undefined<T> = T extends (undefined | infer R) ? R : T
+type test1 = (string | undefined)
+
+type h = wipe_off_undefined<test1>
 
 /** 将选项参数回调风格的函数转为promise ,存在一些 问题，当开启全严格模式时，返回的值是未知的 */
 export function adap_promise<T extends {
@@ -28,7 +33,7 @@ export function adap_promise<T extends {
         /** 获取函数本就该有的约束，不然没有这句话一些必须的参数在adap_promise的par中就不是必须的，这样他们的类型就对不上 */
         & Parameters<T>[0]): any
 }>                                  //un_success_par<T>
-    (fn: T, par: Parameters<T>[0]):Promise<un_success_par<T>> {
+    (fn: T, par: Parameters<T>[0]): Promise<un_success_par<T>> {
     return new Promise((resolve, reject) => {
         fn({
             ...par,
@@ -41,19 +46,4 @@ export function adap_promise<T extends {
         })
     })
 }
-
-interface fun2 {
-    ff<T>(par: T | undefined): T
-}
-
-adap_promise(wx.getSetting, {
-    success(a) {
-
-    }
-}).then(_r => {
-
-})
-type a = undefined | {}
-
-type c = b<a>
 
